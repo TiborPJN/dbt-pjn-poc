4592fd50ba1684325f59354d234df1eff0e53894
Tibor Olah <tibor@payjustnow.com>
2024-01-31 13:29:09 +0000

    adding  _volatile event data

---
+++ dbt_pjn/models/staging/pjnpoc1/stg_pjnpoc1__volatile_consumerordercancelledevent.sql
@@ -0,0 +1,36 @@
+with 
+
+source as (
+
+    select * from {{ source('pjnpoc1', 'volatile_consumerordercancelledevent') }}
+
+),
+
+renamed as (
+
+    select
+        id,
+        origin_event_id,
+        reference,
+        reference_note,
+        type,
+        name,
+        created,
+        received,
+        status,
+        data,
+        version,
+        source_environment,
+        source_module,
+        source_component,
+        source_db_urn,
+        partition_0,
+        partition_1,
+        partition_2,
+        partition_3
+
+    from source
+
+)
+
+select * from renamed
